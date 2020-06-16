ff417d369d3cb46fef14854059cfdce1b64ecd2e
Wolfy-J <wolfy.jd@gmail.com>
2020-06-16 10:23:47 +0300

    - added support for {id} route interpolation

--- src/Directive/RouteDirective.php
+++ src/Directive/RouteDirective.php
@@ -11,11 +11,28 @@ declare(strict_types=1);
 
 namespace Spiral\Stempler\Directive;
 
+use Psr\Container\ContainerInterface;
+use Spiral\Core\Container\SingletonInterface;
+use Spiral\Router\Exception\RouterException;
+use Spiral\Router\Exception\UndefinedRouteException;
+use Spiral\Router\RouterInterface;
 use Spiral\Stempler\Exception\DirectiveException;
 use Spiral\Stempler\Node\Dynamic\Directive;
 
-final class RouteDirective extends AbstractDirective
+final class RouteDirective extends AbstractDirective implements SingletonInterface
 {
+    /** @var ContainerInterface */
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct();
+        $this->container = $container;
+    }
+
     /**
      * Injects service into template.
      *
@@ -32,8 +49,38 @@ final class RouteDirective extends AbstractDirective
         }
 
         return sprintf(
-            '<?php echo $this->container->get(\Spiral\Router\RouterInterface::class)->uri(%s); ?>',
+            '<?php echo $this->container->get(\Spiral\Stempler\Directive\RouteDirective::class)->uri(%s); ?>',
             $directive->body
         );
+    }
+
+    /**
+     * Provides the ability to inject templated args in a form or {id} or {{id}}.
+     *
+     * @param string $route
+     * @param array  $params
+     * @return string
+     */
+    public function uri(string $route, array $params = []): string
+    {
+        $vars = [];
+        $restore = [];
+        foreach ($params as $key => $value) {
+            if (is_string($value) && preg_match('/\{.*\}/', $value)) {
+                $restore[sprintf('__%s__', $key)] = $value;
+                $value = sprintf('__%s__', $key);
+            }
+
+            $vars[$key] = $value;
+        }
+
+        try {
+            return strtr(
+                $this->container->get(RouterInterface::class)->uri($route, $vars)->__toString(),
+                $restore
+            );
+        } catch (UndefinedRouteException $e) {
+            throw new RouterException("No such route {$route}", $e->getCode(), $e);
+        }
     }
 }
